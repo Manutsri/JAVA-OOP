c90dc958358ab319b65d34326bc01527f74e9ca1
Manutsri <manutsri2@gmail.com>
2016-03-29 11:38:11 +0700

    Added files via upload

---
+++ exinstanceMethod2.java
@@ -0,0 +1,23 @@
+class exinstanceMethod2 
+{
+	public static void main(String[] args) 
+	{
+		exinstanceMethod2 str = new exinstaneMethod2();
+		str.print("Result(+,/) = "+(10+5)+","+(10/2d));
+	}
+
+	public void print(String arg)
+	{
+		System.out.println(arg);
+	}
+
+	public void print(int arg)
+	{
+		System.out.println(arg);
+	}
+
+	public void print(double arg)
+	{
+		System.out.println(arg);
+	}
+}
